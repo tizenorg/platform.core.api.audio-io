7a9e8e95bbbfa65085515de603a68b9073f5c139
dh8210.kim <dh8210.kim@samsung.com>
2015-01-15 11:51:39 +0900

    audio-io : Change APIs based on Tizen 2.3
    Change-Id: Ic90535a671f8853ab74c60e8e2da7fd2718b0447
    Signed-off-by: dh8210.kim <dh8210.kim@samsung.com>

--- include/audio_io.h
+++ include/audio_io.h
@@ -90,6 +90,7 @@ typedef enum{
     AUDIO_IO_ERROR_OUT_OF_MEMORY       = TIZEN_ERROR_OUT_OF_MEMORY,     /**< Out of memory */
     AUDIO_IO_ERROR_INVALID_PARAMETER   = TIZEN_ERROR_INVALID_PARAMETER, /**< Invalid parameter */
     AUDIO_IO_ERROR_INVALID_OPERATION   = TIZEN_ERROR_INVALID_OPERATION, /**< Invalid operation */
+	AUDIO_IO_ERROR_PERMISSION_DENIED   = TIZEN_ERROR_PERMISSION_DENIED, /**< Device open error by security */
     AUDIO_IO_ERROR_DEVICE_NOT_OPENED   = AUDIO_IO_ERROR_CLASS | 0x01, /**< Device open error */
     AUDIO_IO_ERROR_DEVICE_NOT_CLOSED   = AUDIO_IO_ERROR_CLASS | 0x02, /**< Device close error */
     AUDIO_IO_ERROR_INVALID_BUFFER      = AUDIO_IO_ERROR_CLASS | 0x03, /**< Invalid buffer pointer */
@@ -101,14 +102,14 @@ typedef enum{
  */
 typedef enum
 {
-    AUDIO_IO_INTERRUPTED_COMPLETED = 0, 				/**< Interrupt completed*/
-    AUDIO_IO_INTERRUPTED_BY_MEDIA, 				/**< Interrupted by non-resumable media application*/
-    AUDIO_IO_INTERRUPTED_BY_CALL,						/**< Interrupted by incoming call*/
-    AUDIO_IO_INTERRUPTED_BY_EARJACK_UNPLUG,			/**< Interrupted by unplugging headphone*/
-    AUDIO_IO_INTERRUPTED_BY_RESOURCE_CONFLICT,		/**< Interrupted by resource conflict*/
-    AUDIO_IO_INTERRUPTED_BY_ALARM,					/**< Interrupted by alarm*/
-    AUDIO_IO_INTERRUPTED_BY_EMERGENCY,					/**< Interrupted by emergency*/
-    AUDIO_IO_INTERRUPTED_BY_RESUMABLE_MEDIA, 				/**< Interrupted by resumable media application*/
+    AUDIO_IO_INTERRUPTED_COMPLETED = 0,         /**< Interrupt completed */
+    AUDIO_IO_INTERRUPTED_BY_MEDIA,              /**< Interrupted by a media application */
+    AUDIO_IO_INTERRUPTED_BY_CALL,               /**< Interrupted by an incoming call */
+    AUDIO_IO_INTERRUPTED_BY_EARJACK_UNPLUG,     /**< Interrupted by unplugging headphones */
+    AUDIO_IO_INTERRUPTED_BY_RESOURCE_CONFLICT,  /**< Interrupted by a resource conflict */
+    AUDIO_IO_INTERRUPTED_BY_ALARM,              /**< Interrupted by an alarm */
+    AUDIO_IO_INTERRUPTED_BY_EMERGENCY,          /**< Interrupted by an emergency */
+    AUDIO_IO_INTERRUPTED_BY_NOTIFICATION,       /**< Interrupted by a notification */
 } audio_io_interrupted_code_e;
 
 /**
@@ -136,6 +137,19 @@ typedef void (*audio_io_interrupted_cb)(audio_io_interrupted_code_e code, void *
 //AUDIO INPUT
 //
 
+/**
+ * @brief Called when audio input data is available in asynchronous(event) mode.
+ *
+ *
+ * @remarks @a use audio_in_peek() to get audio in data inside callback, use audio_in_drop() after use of peeked data.
+ *
+ * @param[in] handle The handle to the audio input
+ * @param[in] nbytes The amount of available audio in data which can be peeked.
+ * @param[in] userdata The user data passed from the callback registration function
+ *
+ * @see audio_in_set_stream_cb()
+ */
+typedef void (*audio_in_stream_cb)(audio_in_h handle, size_t nbytes, void *userdata);
 
 /**
  * @brief    Creates an audio device instance and returns an input handle to record PCM (pulse-code modulation) data
@@ -151,6 +165,7 @@ typedef void (*audio_io_interrupted_cb)(audio_io_interrupted_code_e code, void *
  * @return 0 on success, otherwise a negative error value.
  * @retval #AUDIO_IO_ERROR_NONE Successful
  * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_PERMISSION_DENIED Permission denied
  * @retval #AUDIO_IO_ERROR_OUT_OF_MEMORY Out of memory
  * @retval #AUDIO_IO_ERROR_DEVICE_NOT_OPENED Device not opened
  * @retval #AUDIO_IO_ERROR_SOUND_POLICY    Sound policy error
@@ -158,7 +173,29 @@ typedef void (*audio_io_interrupted_cb)(audio_io_interrupted_code_e code, void *
  */
 int audio_in_create(int sample_rate, audio_channel_e channel, audio_sample_type_e type , audio_in_h *input);
 
-
+/**
+ * @brief Creates an audio loopback device instance and returns an input handle to record PCM (pulse-code modulation) data.
+ *
+ *
+ * @details This function is used for audio loopback input initialization.
+ *
+ * @remarks @a input must be released using audio_in_destroy().
+ *
+ * @param[in] sample_rate	The audio sample rate in 8000[Hz] ~ 48000[Hz]
+ * @param[in] channel	The audio channel type, mono, or stereo
+ * @param[in] type	The type of audio sample (8- or 16-bit)
+ * @param[out] input	An audio input handle will be created, if successful
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_PERMISSION_DENIED Permission denied
+ * @retval #AUDIO_IO_ERROR_OUT_OF_MEMORY Out of memory
+ * @retval #AUDIO_IO_ERROR_DEVICE_NOT_OPENED Device not opened
+ * @retval #AUDIO_IO_ERROR_SOUND_POLICY Sound policy error
+ * @see audio_in_destroy()
+ */
+int audio_in_create_loopback(int sample_rate, audio_channel_e channel, audio_sample_type_e type , audio_in_h* input);
 
 /**
  * @brief    Releases the audio input handle and all its resources associated with an audio stream
@@ -311,6 +348,89 @@ int audio_in_unset_interrupted_cb(audio_in_h input);
  */
 int audio_in_ignore_session(audio_in_h input);
 
+/**
+ * @brief Sets an asynchronous(event) callback function to handle recording PCM (pulse-code modulation) data.
+ *
+ *
+ * @details @a callback will be called when you can read a PCM data.
+ * It might cause dead lock if change the state of audio handle in callback.
+ * (ex: audio_in_destroy, audio_in_prepare, audio_in_unprepare)
+ * Recommend to use as a VOIP only.
+ * Recommend not to hold callback too long.(it affects latency)
+ *
+ * @remarks @a input must be created using audio_in_create().
+ *
+ * @param[in] input    An audio input handle
+ * @param[in] callback notify stream callback when user can read data (#audio_in_stream_cb)
+ * @param[in] userdata user data to be retrieved when callback is called
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_OUT_OF_MEMORY Out of memory
+ * @retval #AUDIO_IO_ERROR_DEVICE_NOT_OPENED Device not opened
+ * @retval #AUDIO_IO_ERROR_SOUND_POLICY Sound policy error
+ *
+ * @see audio_out_set_stream_cb()
+ */
+int audio_in_set_stream_cb(audio_in_h input, audio_in_stream_cb callback, void* userdata);
+
+/**
+ * @brief Unregisters the callback function.
+ *
+ *
+ * @param[in] input The handle to the audio input
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_INVALID_OPERATION Invalid operation
+ *
+ * @see audio_in_set_interrupted_cb()
+ */
+int audio_in_unset_stream_cb(audio_in_h input);
+
+/**
+ * @brief peek from audio in buffer
+ *
+ *
+ * @details This function works correctly only with read, write callback. Otherwise it won't operate as intended.
+ *
+ * @remarks @a Works only in asynchronous(event) mode. This will just retrieve buffer pointer from audio in buffer. Drop after use.
+ *
+ * @param[in] input The handle to the audio input
+ * @param[out] buffer start buffer pointer of peeked audio in data
+ * @param[in,out] length amount of audio in data to be peeked
+ * @return @c 0 on success,
+ *         otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_INVALID_OPERATION Invalid operation
+ *
+ * @see audio_in_drop()
+ */
+int audio_in_peek(audio_in_h input, const void **buffer, unsigned int *length);
+
+/**
+ * @brief drop peeked audio buffer.
+ *
+ * @details This function works correctly only with read, write callback. Otherwise it won't operate as intended.
+ *
+ *
+ * @remarks @a Works only in asynchronous(event) mode. This will remove audio in data from actual stream buffer. Use this if peeked data is not needed anymore.
+ *
+ * @param[in] input The handle to the audio input
+ * @return 0 on success, otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_INVALID_OPERATION Invalid operation
+ *
+ * @see audio_in_peek()
+ */
+int audio_in_drop(audio_in_h input);
+
+
+
 //
 //  AUDIO OUTPUT
 //
@@ -325,9 +445,25 @@ int audio_in_ignore_session(audio_in_h input);
  */
 
 /**
- * @brief    Creates an audio device instance and returns an output handle to play PCM (pulse-code modulation) data
- * @details  This function is used for audio output initialization. 
- * @remarks @a output must be released audio_out_destroy() by you.
+ * @brief Called when audio out data can be written in asynchronous(event) mode.
+ *
+ *
+ * @remarks @a use audio_out_write() to write pcm data inside this callback.
+ * @param[in] handle The handle to the audio output
+ * @param[in] nbytes The amount of audio in data which can be written.
+ * @param[in] userdata The user data passed from the callback registration function
+ *
+ * @see audio_out_set_stream_cb()
+ */
+typedef void (*audio_out_stream_cb)(audio_out_h handle, size_t nbytes, void *userdata);
+
+/**
+ * @brief Creates an audio device instance and returns an output handle to play PCM (pulse-code modulation) data.
+
+ *
+ * @details This function is used for audio output initialization.
+ *
+ * @remarks @a output must be released by audio_out_destroy().
  *
  * @param[in]  sample_rate  The audio sample rate in 8000[Hz] ~ 48000[Hz]
  * @param[in]  channel      The audio channel type, mono, or stereo
@@ -511,7 +647,45 @@ int audio_out_unset_interrupted_cb(audio_out_h output);
  */
 int audio_out_ignore_session(audio_out_h output);
 
+/**
+ * @brief Sets an asynchronous(event) callback function to handle playing PCM (pulse-code modulation) data.
+ *
+ *
+ * @details @a callback will be called when you can write a PCM data.
+ * It might cause dead lock if change the state of audio handle in callback.
+ * (ex: audio_in_destroy, audio_in_prepare, audio_in_unprepare)
+ * Recommend to use as a VOIP only.
+ * Recommend not to hold callback too long.(it affects latency)
+ *
+ * @remarks @a output must be created using audio_out_create().
+ *
+ * @param[in] output   An audio output handle
+ * @param[in] callback notify stream callback when user can write data (#audio_out_stream_cb)
+ * @param[in] userdata user data to be retrieved when callback is called
+ * @return 0 on success, otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_OUT_OF_MEMORY Out of memory
+ * @retval #AUDIO_IO_ERROR_DEVICE_NOT_OPENED Device not opened
+ * @retval #AUDIO_IO_ERROR_SOUND_POLICY Sound policy error
+ *
+ * @see audio_in_set_stream_cb()
+ */
+int audio_out_set_stream_cb(audio_out_h output, audio_out_stream_cb callback, void* userdata);
 
+/**
+ * @brief Unregisters the callback function.
+ *
+ *
+ * @param[in] output The handle to the audio output
+ * @return 0 on success, otherwise a negative error value
+ * @retval #AUDIO_IO_ERROR_NONE Successful
+ * @retval #AUDIO_IO_ERROR_INVALID_PARAMETER Invalid parameter
+ * @retval #AUDIO_IO_ERROR_INVALID_OPERATION Invalid operation
+ *
+ * @see audio_out_set_stream_cb()
+ */
+int audio_out_unset_stream_cb(audio_out_h output);
 /**
  * @}
 */
--- include/audio_io_private.h
+++ include/audio_io_private.h
@@ -20,6 +20,20 @@
 #include <sound_manager.h>
 #include <mm_sound.h>
 
+/*
+* Internal Macros
+*/
+
+#define AUDIO_IO_INTERRUPTED_BY_RESUMABLE_MEDIA       (AUDIO_IO_INTERRUPTED_BY_NOTIFICATION + 1)
+#define AUDIO_IO_INTERRUPTED_BY_RESUMABLE_CANCELED    (AUDIO_IO_INTERRUPTED_BY_NOTIFICATION + 2)
+
+#define AUDIO_IO_CHECK_CONDITION(condition,error,msg)   \
+                if(condition) {} else \
+                { LOGE("[%s] %s(0x%08x)",__FUNCTION__, msg,error); return error;}; \
+
+#define AUDIO_IO_NULL_ARG_CHECK(arg)	\
+	AUDIO_IO_CHECK_CONDITION(arg != NULL, AUDIO_IO_ERROR_INVALID_PARAMETER, "AUDIO_IO_ERROR_INVALID_PARAMETER" )
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -27,16 +41,20 @@ extern "C" {
 
 typedef struct _audio_in_s{
 	MMSoundPcmHandle_t mm_handle;
+
 	int _buffer_size;
 	int _sample_rate;
 	audio_channel_e _channel;
 	audio_sample_type_e _type;
 	audio_io_interrupted_cb user_cb;
 	void* user_data;
+	audio_in_stream_cb stream_cb;
+	void* stream_userdata;
 } audio_in_s;
 
 typedef struct _audio_out_s{
 	MMSoundPcmHandle_t mm_handle;
+
 	int _buffer_size;
 	int _sample_rate;
 	audio_channel_e _channel;
@@ -44,7 +62,23 @@ typedef struct _audio_out_s{
 	sound_type_e	_sound_type;
 	audio_io_interrupted_cb user_cb;
 	void* user_data;
+	audio_out_stream_cb stream_cb;
+	void* stream_userdata;
 } audio_out_s;
+
+int __convert_audio_io_error_code(int code, char *func_name);
+int __check_parameter(int sample_rate, audio_channel_e channel, audio_sample_type_e type);
+int __mm_sound_pcm_capture_msg_cb (int message, void *param, void *user_param);
+audio_io_interrupted_code_e __translate_interrupted_code (int code);
+
+int audio_in_create_private(int sample_rate, audio_channel_e channel, audio_sample_type_e type , audio_in_h* input);
+
+int audio_in_set_callback_private(audio_in_h input, audio_in_stream_cb callback, void* userdata);
+
+int audio_out_create_private(int sample_rate, audio_channel_e channel, audio_sample_type_e type, sound_type_e sound_type, audio_out_h* output);
+
+int audio_out_set_callback_private(audio_out_h output, audio_out_stream_cb callback, void* userdata);
+
 
 #ifdef __cplusplus
 }
